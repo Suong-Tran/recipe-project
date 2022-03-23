43d1a4ad7d41fc1c6041b77fce5e0694638d5f90
Starol Chen <starolchen102@gmail.com>
2022-03-23 00:04:31 -0400

    first commit

---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+
+function Cuisine() {
+    const [cuisine, setCuisine] = useState([]);
+    let param = useParams();
+
+    const getCuisine = async (name) => {
+        const data = await fetch(
+            `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=10&cuisine=${name}`);
+        const recipes = await data.json();
+        setCuisine(recipes.results);
+    };
+
+    useEffect(() => {
+        getCuisine(param.type)
+    }, [param.type]);
+
+    return (
+        <Grid>
+            {cuisine.map((item) => {
+                return (
+                    <Card key={item.id}>
+                        <Link to={'/recipe/' +item.id}>
+                            <img src={item.image} alt="" />
+                            <h4>{item.title}</h4>
+                        </Link>
+                    </Card>
+                )
+            })}
+        </Grid>
+    );
+
+}
+
+/*  Styles for the page */
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem,1fr));
+    grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+    img {
+        width: 100%;
+        border-radius: 2rem;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    h4 {
+        text-align: center;
+        padding: 1rem;
+    }
+`;
+
+export default Cuisine;
